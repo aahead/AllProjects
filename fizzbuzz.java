d0df15c2f5e436314be2d410285f085f040fee04
aahead <145381795+aahead@users.noreply.github.com>
2025-01-15 09:02:53 -0500

    Create fizzbuzz.java

---
+++ fizzbuzz.java
@@ -0,0 +1,51 @@
+public class fizzbuzz {
+  public static void main(String args[]) {
+        int x = 15;
+        int y = 5;
+        int z = 3;
+        int a = 2;
+        
+        if (x % 15 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (x % 5 == 0) {
+            System.out.println("Buzz");
+        } else if (x % 3 == 0) {
+            System.out.println("Fizz");
+        } else {
+            System.out.println("Sorry you weren't fizzy enough");
+        }
+        
+        if (y % 15 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (y % 5 == 0) {
+            System.out.println("Buzz");
+        } else if (y % 3 == 0) {
+            System.out.println("Fizz");
+        } else {
+            System.out.println("Sorry you weren't fizzy enough");
+        }
+        
+        if (z % 15 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (z % 5 == 0) {
+            System.out.println("Buzz");
+        } else if (z % 3 == 0) {
+            System.out.println("Fizz");
+        } else {
+            System.out.println("Sorry you weren't fizzy enough");
+        }
+        
+        if (a % 15 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (a % 5 == 0) {
+            System.out.println("Buzz");
+        } else if (a % 3 == 0) {
+            System.out.println("Fizz");
+        } else {
+            System.out.println("Sorry you weren't fizzy enough");
+        }
+        
+        
+        
+  }
+}
